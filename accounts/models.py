03f2b715aa8c69ea92fa997830119b33b529eceb
Malhar Bonde <113175549+malharrrr@users.noreply.github.com>
2024-11-07 17:26:37 +0530

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.urls import reverse
+from django.templatetags.static import static
+from django_resized import ResizedImageField
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not username:
+            raise ValueError('User must have an username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+
+class Account(AbstractBaseUser):
+    avatar          = ResizedImageField(size=[600, 600], quality=85, upload_to='images/account/avatar/', blank=True, null=True)
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=50)
+    description     = models.TextField(blank=True, null=True)
+    status          = models.BooleanField(default=True, blank=True, null=True)
+
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now_add=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active        = models.BooleanField(default=True)
+    is_superadmin        = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+
+    @property
+    def full_name(self):
+        if not self.first_name and not self.last_name:
+            return self.username
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+    
+    @property
+    def get_avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        return static('/img/default.jpg')
+    
+    @property
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'username': self.username})
+    
